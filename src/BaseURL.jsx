d06d35d27de6d5fffc4e64f04c7514f97aaab058
Sangana Basava M <sanganabasavam1999@gmail.com>
2025-06-23 13:31:29 +0530

    Adedd base url

--- src/BaseURL.jsx
+++ src/BaseURL.jsx
@@ -1,1 +1,5 @@
-export const API_BASE_URL = "http://localhost:5000/api";
+// export const API_BASE_URL = "http://localhost:5000/api";
+
+export const API_BASE_URL = "https://clothly-backend.onrender.com";
+
+
